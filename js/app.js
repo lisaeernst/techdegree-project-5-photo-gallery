8e4b47e17bec5bc522adf1fe4074f301c78a8158
lisaeernst <78939274+lisaeernst@users.noreply.github.com>
2021-04-28 11:20:37 -0700

    adjustments for responsive gallery imgs

--- js/app.js
+++ js/app.js
@@ -20,5 +20,5 @@ const handleSearch = event => {
 }
 
 search.addEventListener('keyup', handleSearch);
-search.addEventListener('search', handleSearch); // clearing the search field resets the gallery
+search.addEventListener('search', handleSearch); // clearing the search field using the X resets the gallery
 
